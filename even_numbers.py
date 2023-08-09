64fb20a1da7090bf58f1c6e5edc0014ae3688d1c
Hreljin <140968167+hreljin@users.noreply.github.com>
2023-08-09 05:47:46 +0000

    Added code to return if even or odd

--- even_numbers.py
+++ even_numbers.py
@@ -1,1 +1,8 @@
-def 
+def is_even(number):
+    return number % 2 == 0
+
+number = int(input("Enter number: "))
+if is_even(number):
+    print(number, "is even")
+else:
+    print(number, "is odd")
